acc995fb011ea02fda4385d90f5f570cb84a07ba
ankital-selldo <ankita.lonkar@sell.do>
2025-03-22 12:52:40 +0530

    Minor change

--- app/uploaders/event_uploader.rb
+++ app/uploaders/event_uploader.rb
@@ -7,11 +7,8 @@ class EventUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
- 
   
   def extension_allowlist
     %w[jpg jpeg gif png]
   end
-
-  
 end
