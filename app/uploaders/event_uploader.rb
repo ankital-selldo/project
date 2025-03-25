a64f9a0f39dd51dcc1d216e7bd9e1b68a2ec3477
ankital-selldo <ankita.lonkar@sell.do>
2025-03-25 15:38:18 +0530

    Image uploading logic updated

--- app/uploaders/event_uploader.rb
+++ app/uploaders/event_uploader.rb
@@ -4,10 +4,11 @@ class EventUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "/opt/render/project/src/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+
   end
 
-  
   def extension_allowlist
     %w[jpg jpeg gif png]
   end
